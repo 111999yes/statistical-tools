10082fe87b3f112de65be32f90388a983019a28c
111999yes <eason1080253@gmail.com>
2026-02-16 00:18:57 +0800

    remove GUI

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QPushButton btn("Qt is working!");
-    btn.resize(300,100);
-    btn.show();
+    QWidget window;
+    window.setFixedSize(400, 300);
+    window.setWindowTitle("統計工具 - Qt 版");
 
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    QPushButton *button = new QPushButton("開始計算統計數據");
+    layout->addWidget(button);
+
+    window.show();
     return app.exec();
 }
